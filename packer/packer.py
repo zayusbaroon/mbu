f10bc71420ce13b3e0d5d699566cd34627a4384f
Zay <zay@zayusbaroon.blog>
2025-01-29 15:47:06 -0500

    fix typos and update .gitignore

--- packer/packer.py
+++ packer/packer.py
@@ -1,4 +1,4 @@
- # Copyright © 2025 Zayus Baroon <zay@zayusbaroon@blog>
+ # Copyright © 2025 Zayus Baroon <zay@zayusbaroon.blog>
 
    # This program is free software: you can redistribute it and/or modify
     #it under the terms of the GNU General Public License as published by
